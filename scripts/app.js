8999c0ee2c515f945ec4384c7abb6749d88d79ed
wagnergoncalvesfilho <wagner.goncalves@escolar.ifrn.edu.br>
2026-08-20 13:22:36 -0300

    finalizacao do arquivo js

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,14 @@
-alert("ola mundo!")
+
+var nasc = 2009;
+let nome = "Wagner";
+const viva = true;
+let altura = 1.83;
+
+if (viva){
+    let saudacao = "ola, " + nome +"!"
+    let msg = "altuta: ${altura}m | idade: ${2026 - nasc}";
+    alert(saudacao+ '\n'+msg);
+}
+else{
+    alert("voce nao esta vivo!")
+}
