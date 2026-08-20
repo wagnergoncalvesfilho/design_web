339d99aa151f5ba17f35b003d69ec6cc59c7db12
wagnergoncalvesfilho <wagner.goncalves@escolar.ifrn.edu.br>
2026-08-20 13:23:42 -0300

    coloquei o ponto e vigula

--- scripts/app.js
+++ scripts/app.js
@@ -5,10 +5,10 @@ const viva = true;
 let altura = 1.83;
 
 if (viva){
-    let saudacao = "ola, " + nome +"!"
+    let saudacao = "ola, " + nome +"!";
     let msg = "altuta: ${altura}m | idade: ${2026 - nasc}";
     alert(saudacao+ '\n'+msg);
 }
 else{
-    alert("voce nao esta vivo!")
+    alert("voce nao esta vivo!");
 }
