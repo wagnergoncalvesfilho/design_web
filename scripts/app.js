59e03d2b32d8f3711787ecb183e60d53c8c1a377
wagnergoncalvesfilho <wagner.goncalves@escolar.ifrn.edu.br>
2026-08-21 14:13:50 -0300

    criacao de variaveis

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,16 @@
-alert("ola mundo!")
+var nasc = 2009;
+let nome = "pedrina";
+const viva = true;
+
+function calcIdade(ano = 2026){
+    let idade = ano -nasc;
+    alert(`Dentro de funcao - Idade ${idade}`);
+    return idade;
+}
+
+calcIdade();
+/* 
+alert(`fora da funcao: Idade: ${idade}`);
+erro, pois a variavel let nao existe fora do escoppo dele
+*/
+alert(`fora da funcao: chamando calcIdade ${calcIdade()}`);
