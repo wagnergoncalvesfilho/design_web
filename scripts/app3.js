22c662d6f065625dcd96a2aaa949368c70823e3f
wagnergoncalvesfilho <wagner.goncalves@escolar.ifrn.edu.br>
2026-08-26 17:56:15 -0300

    aula 4

---
+++ scripts/app3.js
@@ -0,0 +1,15 @@
+let nasc = 2009;
+let nome ="wagner";
+let viva =true;
+let altura = 1.83
+let sexo;
+let peso = null;
+let imc = "peso"*10;
+
+alert(`Ano de nascimento - tipo ${typeof(nasc)}`);
+alert(`nome- tipo: ${typeof(nome)}`);
+alert(`viva - tipo: ${typeof(viva)}`);
+alert(`altura - tipo: ${typeof(altura)}`);
+alert(`sexo - tipo: ${typeof(sexo)}`);
+alert(`peso - tipo: ${typeof(peso)}`);
+alert(`imc - tipo: ${typeof (imc)}`);
