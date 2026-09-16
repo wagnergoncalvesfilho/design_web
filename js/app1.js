1d945db879421b79bbca830e6f29ac781929a02f
wagnergoncalvesfilho <wagner.goncalves@escolar.ifrn.edu.br>
2026-09-16 17:54:58 -0300

    interacao HTML com JS(parte 2)

--- js/app1.js
+++ js/app1.js
@@ -1,23 +1,82 @@
 let i = 1;
 const imagem = document.getElementById("foto");
-
+/*interacao de avancar imagem ou voltar*/
 function proximo(e){
     e.preventDefault();
     if (i < 6){
-        i += 1
+        i = i + 1;
     }
-    imagem.setAttribute('src', `./img/foto${i}.jpg`);
+    imagem.setAttribute("src",`./img/foto${i}.jpg`);
 }
+
 const prox = document.getElementById("prox");
 prox.addEventListener("click", proximo);
 
 function anterior(e){
     e.preventDefault();
-    if (i > 1){
-        i -= 1
+    if(i>1){
+        i = i - 1;
     }
-    imagem.setAttribute('src', `./img/foto${i}.jpg`);
+    imagem.setAttribute("src",`./img/foto${i}.jpg`);
 }
+const ant = document.getElementById("ant");
+ant.addEventListener("click", anterior);
 
-const ant = document.getElementById("ant")
-ant.addEventListener("click", anterior)
+/*interacao de selecao de imagem*/
+function selecionar(e){
+    e.preventDefault();
+    i = Number(cbbox.value);
+    imagem.setAttribute("src",'./img/foto${i}.jpg');
+}
+const cbbox = document.getElementById("combobox");
+cbbox.addEventListener("change", selecionar);
+
+/// interagir com a opacidade
+const opacidade = document.getElementById("opacidade");
+opacidade.addEventListener("input", function(e){
+    e.preventDefault();
+    let valor = opacidade.value;
+    imagem.setAttribute("style", `opacity:${valor}`);
+});
+
+/*interacao js com li*/
+const li1 = document.getElementById('li1');
+li1.addEventListener("mouseover", function(e){
+    e.preventDefault();
+    i = 1;
+    imagem.setAttribute("src", `./img/foto${i}.jpg`)
+})
+
+const li2 = document.getElementById('li2');
+li2.addEventListener("mouseover", function(e){
+    e.preventDefault();
+    i = 2;
+    imagem.setAttribute("src", `./img/foto${i}.jpg`)
+})
+
+const li3 = document.getElementById('li3');
+li3.addEventListener("mouseover", function(e){
+    e.preventDefault();
+    i = 3;
+    imagem.setAttribute("src", `./img/foto${i}.jpg`)
+})
+
+const li4 = document.getElementById('li4');
+li4.addEventListener("mouseover", function(e){
+    e.preventDefault();
+    i = 4;
+    imagem.setAttribute("src", `./img/foto${i}.jpg`)
+})
+
+const li5 = document.getElementById('li5');
+li5.addEventListener("mouseover", function(e){
+    e.preventDefault();
+    i = 5;
+    imagem.setAttribute("src", `./img/foto${i}.jpg`)
+})
+const li6 = document.getElementById('li6');
+li6.addEventListener("mouseover", function(e){
+    e.preventDefault();
+    i = 6;
+    imagem.setAttribute("src", `./img/foto${i}.jpg`)
+})
